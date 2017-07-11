6fbad473b0132bb7d960a9e6bff01b6daa172a04
Rich Middleditch <rich@witchert.co.uk>
2017-07-11 11:11:00 +0100

    Move output dir so its github pages compatible

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var styleguide = require('sc5-styleguide');
 var sass = require('gulp-sass');
-var outputPath = 'output';
+var outputPath = 'docs';
 
 gulp.task('styleguide:generate', function() {
     gulp.src(['fonts/**'])
