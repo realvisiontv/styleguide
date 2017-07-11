abccf66ed444366e60ce187e8521d23f7f3ed78d
Rich Middleditch <rich@witchert.co.uk>
2017-07-11 11:09:31 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+var styleguide = require('sc5-styleguide');
+var sass = require('gulp-sass');
+var outputPath = 'output';
+
+gulp.task('styleguide:generate', function() {
+    gulp.src(['fonts/**'])
+        // Do image sprites, optimizations etc.
+        .pipe(gulp.dest('./fonts'))
+        .pipe(gulp.dest(outputPath + '/fonts'));
+
+    return gulp.src('sass/*.scss')
+        .pipe(styleguide.generate({
+            title: 'Real Vision Styleguide',
+            server: true,
+            rootPath: outputPath,
+            overviewPath: 'README.md'
+        }))
+        .pipe(gulp.dest(outputPath));
+});
+
+gulp.task('styleguide:applystyles', function() {
+    return gulp.src('sass/default.scss')
+        .pipe(sass({
+            errLogToConsole: true
+        }))
+        .pipe(styleguide.applyStyles())
+        .pipe(gulp.dest(outputPath));
+});
+
+gulp.task('watch', ['styleguide'], function() {
+    // Start watching changes and update styleguide whenever changes are detected
+    // Styleguide automatically detects existing server instance
+    gulp.watch(['sass/*.scss'], ['styleguide']);
+});
+
+gulp.task('styleguide', ['styleguide:generate', 'styleguide:applystyles']);
