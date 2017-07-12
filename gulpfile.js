e684d4dbbc969ebcc28a8b06a8747e77f214b045
Rich Middleditch <rich@witchert.co.uk>
2017-07-12 10:18:41 +0100

    Cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -40,7 +40,7 @@ gulp.task('watch', ['styleguide'], function() {
     var app = server(options);
 
     app.listen(3000, function() {
-        console.log('Express server listening on port 3000.');
+        console.log('Express server listening on port 3000. http://localhost:3000');
     });
 });
 
